95bb65f5a97b2e655493501363b440b0f0218041
Manjunath <manju05cs402@yahoo.co.in>
2017-02-07 13:15:02 +0530

    Included Localizable Strings and Utilities class

---
+++ MCAProtect/MCAProtect/Utilities/MCAUtilities.swift
@@ -0,0 +1,13 @@
+//
+//  MCAUtilities.swift
+//  MCAProtect
+//
+//  Created by Manjunath on 07/02/17.
+//  Copyright © 2017 Accionlabs. All rights reserved.
+//
+
+import UIKit
+
+class MCAUtilities: NSObject {
+
+}
