ebf618bbf86e5043d98997fd8adcc254f24f0572
Vinay Kumar <vinaykumar.n@accionlabs.com>
2017-02-09 21:54:12 +0530

    changes done on forgot password

--- MCAProtect/MCAProtect/Controllers/ForgotPassword/MCAForgotPasswordVC.swift
+++ MCAProtect/MCAProtect/Controllers/ForgotPassword/MCAForgotPasswordVC.swift
@@ -8,10 +8,15 @@
 
 import UIKit
 
-class MCAForgotPasswordVC: MCABaseViewController {
+class MCAForgotPasswordVC: MCABaseViewController,UITextFieldDelegate {
+    
+    @IBOutlet weak var scrollView : UIScrollView!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name:NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name:NSNotification.Name.UIKeyboardWillHide, object: nil)
         self.navigationController?.navigationBar.isHidden = false;
         // Do any additional setup after loading the view.
     }
@@ -21,6 +26,35 @@ class MCAForgotPasswordVC: MCABaseViewController {
         // Dispose of any resources that can be recreated.
     }
     
+
+    func keyboardWillShow(notification:NSNotification){
+        //give room at the bottom of the scroll view, so it doesn't cover up anything the user needs to tap
+        var userInfo = notification.userInfo!
+        var keyboardFrame:CGRect = (userInfo[UIKeyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+        keyboardFrame = self.view.convert(keyboardFrame, from: nil)
+        
+        var contentInset:UIEdgeInsets = scrollView.contentInset
+        contentInset.bottom = keyboardFrame.size.height
+        scrollView.contentInset = contentInset
+    }
+    
+    func keyboardWillHide(notification:NSNotification){
+        let contentInset:UIEdgeInsets = UIEdgeInsets.zero
+        scrollView.contentInset = contentInset
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        let contentInset:UIEdgeInsets = UIEdgeInsets.zero
+        scrollView.contentInset = contentInset
+        scrollView.contentOffset = CGPoint(x: 0, y: -60)
+        return true
+    }
+
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true);
+    }
 
     /*
     // MARK: - Navigation
