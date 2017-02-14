ff9ad93fa3d4eb5b2e28c9770a3b50b0cb1f9941
Vinay Kumar <vinaykumar.n@accionlabs.com>
2017-02-14 15:54:17 +0530

    slide menu pod file added

--- MCAProtect/Pods/Target Support Files/Pods-MCAProtect/Pods-MCAProtect-frameworks.sh
+++ MCAProtect/Pods/Target Support Files/Pods-MCAProtect/Pods-MCAProtect-frameworks.sh
@@ -87,9 +87,11 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
   install_framework "$BUILT_PRODUCTS_DIR/JVFloatLabeledTextField/JVFloatLabeledTextField.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SDWebImage/SDWebImage.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/iOS-Slide-Menu/iOS_Slide_Menu.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
   install_framework "$BUILT_PRODUCTS_DIR/JVFloatLabeledTextField/JVFloatLabeledTextField.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SDWebImage/SDWebImage.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/iOS-Slide-Menu/iOS_Slide_Menu.framework"
 fi
