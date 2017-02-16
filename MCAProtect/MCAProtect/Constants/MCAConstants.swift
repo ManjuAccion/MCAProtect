78b9184c67ffa0ec25329ef0412f6ddc199ec911
Sarath NS <sarath@accionlabs.com>
2017-02-16 17:51:35 +0530

    Added color constants.

--- MCAProtect/MCAProtect/Constants/MCAConstants.swift
+++ MCAProtect/MCAProtect/Constants/MCAConstants.swift
@@ -8,6 +8,26 @@
 
 import UIKit
 
+struct ColorConstants {
+    
+    static let red                  = MCAUtilities.hexStringToUIColor(hexaDecimalString:"#A32A2A")
+    
+    static let grey                 = MCAUtilities.hexStringToUIColor(hexaDecimalString:"#4B4B4B")
+    static let greyAlpha80          = MCAUtilities.hexStringToUIColor(hexaDecimalString:"#6F6F6F")
+    static let greyAlpha60          = MCAUtilities.hexStringToUIColor(hexaDecimalString:"#939393")
+    static let greyAlpha40          = MCAUtilities.hexStringToUIColor(hexaDecimalString:"#B7B7B7")
+    static let greyAlpha20          = MCAUtilities.hexStringToUIColor(hexaDecimalString:"#DBDBDB")
+    
+    static let blue                 = MCAUtilities.hexStringToUIColor(hexaDecimalString:"#2D3943")
+    static let blueAlpha80          = MCAUtilities.hexStringToUIColor(hexaDecimalString:"#576169")
+    static let blueAlpha60          = MCAUtilities.hexStringToUIColor(hexaDecimalString:"#81888E")
+    static let blueAlpha40          = MCAUtilities.hexStringToUIColor(hexaDecimalString:"#ABB0B4")
+    static let blueAlpha20          = MCAUtilities.hexStringToUIColor(hexaDecimalString:"#D5D7D9")
+
+    static let green                = MCAUtilities.hexStringToUIColor(hexaDecimalString:"#339966")
+    
+}
+
 
 
 
