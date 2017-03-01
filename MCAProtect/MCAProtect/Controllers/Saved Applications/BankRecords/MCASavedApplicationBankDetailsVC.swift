434e0aa8db449a163c55c608047650465c65505d
Sarath NS <sarath@accionlabs.com>
2017-03-01 16:54:45 +0530

    Added Bank Records and Bank Detail screens

---
+++ MCAProtect/MCAProtect/Controllers/Saved Applications/BankRecords/MCASavedApplicationBankDetailsVC.swift
@@ -0,0 +1,63 @@
+//
+//  MCASavedApplicationBankDetailsVC.swift
+//  MCAProtect
+//
+//  Created by Sarath NS on 3/1/17.
+//  Copyright © 2017 Accionlabs. All rights reserved.
+//
+
+import UIKit
+
+class MCASavedApplicationBankDetailsVC: MCABaseViewController {
+
+    
+    let bankDetailsArray = ["Account Number","Range","Statement Period","Number of Deposits","Deposit Amount","Days with Negative Balance","Average Daily Balance"]
+    let bankDetailsValueArray  = ["BOA546897236","1st Nov 2016 31st Dec 2016","1st-Nov-2016_31st-Dec-2016.pdf","20","$75,000","0","$3,300"]
+    
+    //MARK: View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        tableView.register(UINib(nibName: "MCASavedApplicationsBankDetailsTVCell", bundle: Bundle.main), forCellReuseIdentifier: CellIdentifiers.MCASavedApplicationsBankDetailsTVCell)
+//        tableView.tableFooterView = UIView()
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem.init(image: UIImage(named:"iconEdit"), style: UIBarButtonItemStyle.plain, target: self, action: #selector(editButtonTapped))
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    //MARK: - Table View Datasource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return bankDetailsArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: CellIdentifiers.MCASavedApplicationsBankDetailsTVCell, for: indexPath) as! MCASavedApplicationsBankDetailsTVCell
+        cell.selectionStyle = .none
+        
+        cell.headingLabel.text = bankDetailsArray[indexPath.row]
+        cell.dataTF.text = bankDetailsValueArray[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
+    {
+        return 66.0
+    }
+    
+    func editButtonTapped() {
+        
+    }
+    
+    
+    
+   
+}
