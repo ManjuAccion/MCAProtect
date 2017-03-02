4a605b148a477533a8d1c16c27d3ecac02742307
Sarath NS <sarath@accionlabs.com>
2017-03-02 11:43:50 +0530

    Added action for edit button in Loan and Business Detail screen.

--- MCAProtect/MCAProtect/Controllers/Saved Applications/SavedApplicationDetail/MCASavedApplicationDetailVC.swift
+++ MCAProtect/MCAProtect/Controllers/Saved Applications/SavedApplicationDetail/MCASavedApplicationDetailVC.swift
@@ -8,18 +8,25 @@
 
 import UIKit
 
-class MCASavedApplicationDetailVC: MCABaseViewController,UITableViewDataSource,UITableViewDelegate {
+class MCASavedApplicationDetailVC: MCABaseViewController,UITableViewDataSource,UITableViewDelegate,UITextFieldDelegate {
     
     @IBOutlet weak var tableView: UITableView!
     
     var applicaionDetailType: NSInteger!
+    var isViewingMode : Bool?
+    var summaryTVCell : MCAApplicationSummaryTVCell?
     
     var loanDataSource = ["Business Name","Credit Score","Loan Amount","Loan Term","Need it By"]
+    var loanDataSourceValue = ["Stacy's Boutique","552","$75,000","6 Months","5 Days"]
 
     var businessInformationDataSource = ["Legal Business Name","Contact Name","Telephone","Email","Federal Tax ID","Gross Annual Sales","Business Entity Type","DBA Business Name","State of Incorporation","Business Start Date","Industry Type","Seasonal Business"]
+    var businessInformationDataSourceValue = ["Stacy's Boutique","Helen Parker","(876) 965-8756","helen.parker@gmail.com","8768968","$30,000","Partnership","Stacy's Boutique","California","2014-12-09","Ice Creanm","Yes"]
     var businessAddressDataSource = ["Street","City","State","Zip Code","Web Address","Telephone","Fax Number"]
+    var businessAddressDataSourceValue = ["E 76th PI","Los Angeles","California","780098","www.stacysboutique.com","(976) 745-3435","(976) 745-3435"]
     
     var dataSourceArray : [String] = []
+    var dataSourceValueArray : [String] = []
+
     
     //MARK: - View Life Cycle
     
@@ -27,7 +34,8 @@ class MCASavedApplicationDetailVC: MCABaseViewController,UITableViewDataSource,U
         super.viewDidLoad()
          tableView.register(UINib(nibName: "MCAApplicationSummaryTVCell", bundle: Bundle.main), forCellReuseIdentifier: CellIdentifiers.MCAApplicationSummaryTVCell)
         tableView.tableFooterView = UIView()
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem.init(image: UIImage(named:"iconEdit"), style: UIBarButtonItemStyle.plain, target: self, action: #selector(editButtonTapped))
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(named:"iconEdit"), style: UIBarButtonItemStyle.plain, target: self, action: #selector(editButtonTapped))
+        isViewingMode = true
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -38,12 +46,18 @@ class MCASavedApplicationDetailVC: MCABaseViewController,UITableViewDataSource,U
         case SavedApplicationForm.LoanDetails.rawValue:
             self.title = "Loan Details"
             dataSourceArray = loanDataSource
+            dataSourceValueArray = loanDataSourceValue
+            
         case SavedApplicationForm.BusinessInformation.rawValue:
             self.title = "Business Information"
             dataSourceArray = businessInformationDataSource
+            dataSourceValueArray = businessInformationDataSourceValue
+
         case SavedApplicationForm.BusinessAddress.rawValue:
             self.title = "Business Address"
             dataSourceArray = businessAddressDataSource
+            dataSourceValueArray = businessAddressDataSourceValue
+
         default:
             break
         }
@@ -66,12 +80,15 @@ class MCASavedApplicationDetailVC: MCABaseViewController,UITableViewDataSource,U
         cell.backgroundColor = UIColor.clear
         
         cell.titleLabel.text = dataSourceArray[indexPath.row]
-        cell.dataTF.text = dataSourceArray[indexPath.row]
+        cell.dataTF.text = dataSourceValueArray[indexPath.row]
+        cell.dataTF.delegate = self
+        cell.dataTF.isUserInteractionEnabled = true
         
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
@@ -81,6 +98,33 @@ class MCASavedApplicationDetailVC: MCABaseViewController,UITableViewDataSource,U
     
     func editButtonTapped() {
         
+        if isViewingMode == false {
+            
+            isViewingMode = true
+            tableView.separatorStyle = UITableViewCellSeparatorStyle.none
+            self.view.endEditing(true)
+
+            var image = UIImage(named: "iconEdit")
+            image = image?.withRenderingMode(UIImageRenderingMode.alwaysOriginal)
+            self.navigationItem.rightBarButtonItem = UIBarButtonItem(image:image, style: UIBarButtonItemStyle.plain, target: self, action: #selector(editButtonTapped))
+
+        }
+        else {
+            
+            isViewingMode = false
+            tableView.separatorStyle = UITableViewCellSeparatorStyle.singleLine
+
+            var image = UIImage(named: "iconCheck")
+            image = image?.withRenderingMode(UIImageRenderingMode.alwaysOriginal)
+            self.navigationItem.rightBarButtonItem = UIBarButtonItem(image:image, style: UIBarButtonItemStyle.plain, target: self, action: #selector(editButtonTapped))
+        }
+
     }
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        return !isViewingMode!
+    }
+    
+
     
 }
