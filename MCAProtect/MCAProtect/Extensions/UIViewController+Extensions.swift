794bc21b05ae8b270039119e302d4986d12979bb
Sarath NS <sarath@accionlabs.com>
2017-03-20 11:37:31 +0530

    Moved extension from base view controller to extension class.

--- MCAProtect/MCAProtect/Extensions/UIViewController+Extensions.swift
+++ MCAProtect/MCAProtect/Extensions/UIViewController+Extensions.swift
@@ -43,4 +43,14 @@ extension UIViewController {
     func dismissKeyboard() {
         view.endEditing(true)
     }
+    
+    func presentAlertWithTitle(title: String, message : String)
+    {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let OKAction = UIAlertAction(title: "OK", style: .default) {
+            (action: UIAlertAction) in print("Youve pressed OK Button")
+        }
+        alertController.addAction(OKAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
 }
