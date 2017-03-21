9be4200cb0d4328e61791e1dbc7a9db185f5b5d9
Sarath NS <sarath@accionlabs.com>
2017-03-21 18:26:27 +0530

    Added model classes form MCA Loan Detail

---
+++ MCAProtect/MCAProtect/Controllers/Saved Applications/MCALoans/Loan Detail/Model/MCAExistingLoanDetail.swift
@@ -0,0 +1,30 @@
+//
+//  MCAExistingLoanDetail.swift
+//  MCAProtect
+//
+//  Created by Sarath NS on 3/21/17.
+//  Copyright © 2017 Accionlabs. All rights reserved.
+//
+
+import UIKit
+
+class MCAExistingLoanDetail: NSObject {
+    
+    var funderName : String?
+    var fundedAmount : Float?
+    var currentBalance : Float?
+    var loanTerm : Int?
+    var frequency : String?
+    var amount : Float?
+    
+    init(data: NSDictionary?) {
+        
+        if nil == data {
+            
+        }
+        else {
+            
+        }
+    }
+
+}
